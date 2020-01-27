f87bdc733fe80bc15d0584ad5a546196d8df6b0b
anhdung <anhdung@wakumo.vn>
2020-01-27 15:22:07 +0700

    make import method

--- app/controllers/flash_cards_controller.rb
+++ app/controllers/flash_cards_controller.rb
@@ -24,23 +24,30 @@ class FlashCardsController < ApplicationController
     def import
         data = FlashCard.import_file
         sheet2 = data.sheet 2
-        headers = sheet2.row(1)
+        importData((2..271), sheet2)
+    end
+    private
+    def importData(arr, sheet)
+        headers = sheet.row(1)
         word_index = headers.index('word')
         hira_index = headers.index('hiragana')
         meaning_index = headers.index('meaning_vi')
         cards = []
         cards_translations = []
-        (2..sheet2.last_row).to_a.each do |i|
-            card = {word: sheet2.row(i)[word_index], hiragana: sheet2.row(i)[hira_index], lesson_id: params[:lesson_id]}
+        arr.to_a.each do |i|
+            binding.pry
+            card = {word: sheet.row(i)[word_index], hiragana: sheet.row(i)[hira_index], lesson_id: params[:lesson_id]}
             flashcard = FlashCard.create(card)
-            card_meaning = {meaning: sheet2.row(i)[meaning_index], language_id: params[:language_id], flash_card_id: flashcard.id}
-            card_translation = CardTranslation.create(card_meaning)
+            card_meaning = {meaning: sheet.row(i)[meaning_index], language_id: params[:language_id], flash_card_id: flashcard.id}
+            # card_translation = CardTranslation.create(card_meaning)
             # @flashcard.new()
         end
     end
-    private
     def flashcard_params
         params.permit(:word, :hiragana,:lesson_id, :sino, :ipa, :example)
+    end
+    def import_params
+        # params.permit(:, :hiragana,:lesson_id, :sino, :ipa, :example)
     end
     def render_paging_json flashcards
         meta = {}
