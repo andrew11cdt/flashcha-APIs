ea2d675cbe71091aac01bb00d28ab28bd86d2223
judy <ngan.bui@ryerson.ca>
2020-04-28 10:59:58 +0700

    fixed - not optimal

--- app/models/language.rb
+++ app/models/language.rb
@@ -1,2 +1,10 @@
 class Language < ApplicationRecord
+    validates :name, :lang_code,
+    uniqueness: {
+        scope: [:name, :lang_code],
+        case_sensitive: false,
+        message: "^This language already exits!",
+        status: 409
+      },
+      on: :create
 end
